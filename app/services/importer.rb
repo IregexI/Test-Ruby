5e4fd96adb09060463c8b9f9434b7bce0dc35dc6
Regex <regex.git@gmail.com>
2023-02-13 16:11:47 +0300

    delete in importer.rb(to_s, require csv)

--- app/services/importer.rb
+++ app/services/importer.rb
@@ -1,8 +1,7 @@
-require 'csv'
 table = CSV.parse(File.read("articles.csv"))
 
 table.each do |t, b|
-  @article = Article.new(title: t.to_s, body: b.to_s)
+  @article = Article.new(title: t,body: b)
   @article.save
 end
 #@article = Article.new(title: , body: )
